41fc2ab8704eecce7be617e999ddb49ea43b1bd9
divpal-007 <davezaxter@gmail.com>
2026-04-27 17:18:25 +0530

    CORS issue fix

--- src/api/ticketApi.js
+++ src/api/ticketApi.js
@@ -2,10 +2,17 @@ import axios from "axios";
 // Connecting backend CRUD operations with React using Spring
 // Centralized API calls
 const API = "https://retiring-huntress-brownnose.ngrok-free.dev/api/tickets";
+const api = axios.create({
+  baseURL: 'https://retiring-huntress-brownnose.ngrok-free.dev',
+  headers: {
+    'ngrok-skip-browser-warning': 'true',
+    'Content-Type': 'application/json',
+  },
+});
 
 export const createTicket = (data) => axios.post(API, data);
 
-export const getTickets = () => axios.get(API);
+export const getTickets = () => api.get("/api/tickets");
 
 export const updateTicket = (id, data) =>
   axios.put(`${API}/${id}`, data);

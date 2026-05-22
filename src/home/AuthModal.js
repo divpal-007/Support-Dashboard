fb868422590a0efdc75ee80e78430e1c552e114a
divpal-007 <davezaxter@gmail.com>
2026-05-22 22:47:02 +0530

    v3 changes

--- src/home/AuthModal.js
+++ src/home/AuthModal.js
@@ -366,7 +366,7 @@ function CreateForm({ onSwitch }) {
     <div>
       <div style={{ marginBottom: "20px" }}>
         <div style={{ fontSize: "24px", fontWeight: "700", color: TEXT_HI, letterSpacing: "-0.025em", marginBottom: "5px" }}>Create your workspace</div>
-        <div style={{ fontSize: "13.5px", color: TEXT_LOW }}>Get started with the Service</div>
+        {/* <div style={{ fontSize: "13.5px", color: TEXT_LOW }}>Get started with the Service</div> */}
       </div>
 
       <div style={{ display: "flex", gap: "8px", marginBottom: "8px" }}>

759e2d4be122a1fd4de3d1513ca550704e97ed1d
divpal-007 <davezaxter@gmail.com>
2026-05-28 12:33:31 +0530

    V3.0.0 uupdates and enhancements

--- src/services/authService.js
+++ src/services/authService.js
@@ -1,7 +1,7 @@
 import axios from "axios";
 
 const api = axios.create({
-    baseURL: process.env.REACT_APP_API_URL || 'http://13.126.196.238:8080/api',
+    baseURL: process.env.REACT_APP_API_URL || 'http://13.126.196.238:8080/api', //new changes
     withCredentials: true,
     headers: {
         'Content-Type':'application/json'
